639eeadc9451cca774d25677ec770b070c0f83f5
liuliu0306 <996125554@qq.com>
2022-05-08 10:21:46 +0800

    Update unrolled_linked_list_test.py

--- unrolled_linked_list_test.py
+++ unrolled_linked_list_test.py
@@ -14,7 +14,6 @@ class TestMutableULList(unittest.TestCase):
     # first we use unit test!
 
     def test_add_and_remove(self):
-        print('\nTesting add and remove...')
         ull = UnrolledLinkedList()
         ull.add(1)
         self.assertEqual(ull.to_list(), [1])
@@ -26,12 +25,10 @@ class TestMutableULList(unittest.TestCase):
         self.assertEqual(ull.to_list(), [])
 
     def test_traversal(self):
-        print('Testing traversal...')
         ull = UnrolledLinkedList(4)
         self.assertEqual(ull.traversal(), None)
 
     def test_to_list(self):
-        print('Testing to_list...')
         ull = UnrolledLinkedList(4)
         ull.add(1)
         ull.add(2)
@@ -39,13 +36,11 @@ class TestMutableULList(unittest.TestCase):
         self.assertEqual(type(ull.to_list()), list)
 
     def test_from_list(self):
-        print('Testing from_list...')
         dlist = [1, 2, 3, 4]
         ull = UnrolledLinkedList(4)
         self.assertEqual(ull.from_list(dlist), None)
 
     def test_size(self):
-        print('Testing size...')
         ull = UnrolledLinkedList(4)
         ull.add(1)
         ull.add(2)
@@ -54,34 +49,29 @@ class TestMutableULList(unittest.TestCase):
         self.assertEqual(ull.size(), 4)
 
     def test_set_element(self):
-        print('Testing set_element...')
         ull = UnrolledLinkedList(4)
         lit = [1, 2, 3, 4]
         ull.from_list(lit)
         self.assertEqual(ull.set_element(1, 999).to_list(), [1, 999, 3, 4])
 
     def test_ull_filter(self):
-        print('Testing ull_filter...')
         ull = UnrolledLinkedList(4)
         ull.from_list([1, 2, 3, 4])
         ull.ull_filter()
         self.assertEqual(ull.to_list(), [2, 4])
 
     def test_ull_map(self):
-        print('Testing ull_map...')
         ull = UnrolledLinkedList(4)
         ull.from_list([1, 2, 3, 4])
         ull.ull_map(lambda x: x**2)
         self.assertEqual(ull.to_list(), [1, 4, 9, 16])
 
     def test_ull_reduce(self):
-        print('Testing ull_reduce...')
         ull = UnrolledLinkedList(4)
         ull.from_list([1, 2, 3, 4])
         self.assertEqual(ull.ull_reduce(lambda x, y: x+y), 10)
 
     def test_iterate(self):
-        print('Testing iterate...')
         ull = UnrolledLinkedList()
         ull.from_list([1, 2, 3, 4])
         iter1 = iter(ull)
@@ -93,14 +83,12 @@ class TestMutableULList(unittest.TestCase):
             self.assertEqual(next(iter2), arr[cyi])
 
     def test_reverse(self):
-        print('Testing reverse...')
         ull = UnrolledLinkedList(4)
         ull.from_list([1, 2, 3, 4])
         b = ull.reverse()
         self.assertEqual(b.to_list(), [4, 3, 2, 1])
 
     def test_is_member(self):
-        print('Testing is_member...')
         ull = UnrolledLinkedList(4)
         ull.from_list([1, 2, 3, 4])
         b = ull.is_member(3)
@@ -109,7 +97,6 @@ class TestMutableULList(unittest.TestCase):
         self.assertEqual(b, False)
 
     def test_empty(self):
-        print('Testing empty...')
         ull = UnrolledLinkedList()
         ull.from_list([1, 2, 3, 4])
         b = ull.empty()
@@ -117,22 +104,17 @@ class TestMutableULList(unittest.TestCase):
 
     # new: test which demonstrates how capacity can increase.
     def test_capacity(self):
-        print('Testing capacity ...')
         ull = UnrolledLinkedList()
         self.assertEqual(ull.capacity, 4)
-
-        ull1 = UnrolledLinkedList(6)
-        self.assertEqual(ull1.capacity, 6)
-
-        ull2 = UnrolledLinkedList(3)
-        self.assertEqual(ull2.capacity, 3)
+        # dynamically extended
+        ull.capacity = 6
+        self.assertEqual(ull.capacity, 6)
 
 # ---------------------------------------------
     # then we use PBT test!
     @settings(max_examples=10)
     @given(st.lists(st.integers()))
     def test_concat_PBT(self, a):
-        print('Testing concat PBT...')
         ull = UnrolledLinkedList()
         ullcp = UnrolledLinkedList()
         ull.from_list([1, 2, 3])
@@ -143,8 +125,7 @@ class TestMutableULList(unittest.TestCase):
 
     @settings(max_examples=10)
     @given(st.lists(st.integers()))
-    def test_monoid_properties_PBT(self, a):
-        print('Testing monoid properties PBT...')
+    def test_monoid_identity_PBT(self, a):
         ull1 = UnrolledLinkedList()
         ull2 = UnrolledLinkedList()
         ull3 = UnrolledLinkedList()
@@ -155,22 +136,20 @@ class TestMutableULList(unittest.TestCase):
         ie1 = ull1.concat(ull2, empty=ull1).to_list()
         ie2 = ull2.concat(ull1, empty=ull1).to_list()
         self.assertEqual(ie1, ie2)
-        # test associativity
-        obj1 = ull1.concat(ull2).concat(ull3).to_list()
-        obj2 = ull2.concat(ull3).concat(ull1).to_list()
-        self.assertEqual(obj1, obj2)
 
     # new: Test associativity, in sequence of concatenations.
     @settings(max_examples=10)
     @given(st.lists(st.integers()))
     def test_monoid_associativity_PBT(self, a):
-        print('Testing monoid associativity PBT...')
-        ull1 = UnrolledLinkedList()
-        ull2 = UnrolledLinkedList()
-        ull1.from_list(a[:5])
-        ull2.from_list(a[5:])
-        ie1 = ull1.concat(ull2).to_list()
-        self.assertEqual(ie1, a)
+        ull_a = UnrolledLinkedList()
+        ull_b = UnrolledLinkedList()
+        ull_c = UnrolledLinkedList()
+        ull_a.from_list(a[:3])
+        ull_b.from_list(a[3:7])
+        ull_c.from_list(a[7:])
+        ie1 = ull_a.concat(ull_b).concat(ull_c).to_list()
+        ie2 = ull_b.concat(ull_c).concat(ull_a).to_list()
+        self.assertEqual(ie1, ie2)
 
 
 if __name__ == '__main__':
